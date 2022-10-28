dba063831d228a1c87771929407fdfb65a54d60e
ryansalty <rsoety@gmail.com>
2022-10-28 14:54:49 -0400

    Implement functioning pages for each tab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import pizzaImage from './imgs/pizza.jpg';
+import cheesePizzaPic from './imgs/cheese.jpg';
+import pepPizzaPic from './imgs/pep.jpg';
+import meatPizzaPic from './imgs/meat.jpg';
+
+
+
 
 const firstLoad = (() => {
 
@@ -17,7 +23,8 @@ const homePage = (() => {
     const content = document.getElementById("content");
 
     const load = () => {
-        console.log("in load of homepage");
+
+        console.log("in home load");
         //Create div container for headline and the headline
         const headlineContainer = document.createElement("div");
         headlineContainer.className = "container";
@@ -27,6 +34,7 @@ const homePage = (() => {
         headlineContainer.appendChild(headline);
         content.appendChild(headlineContainer);
 
+        //Create div container for picture and the picture
         const picContainer = document.createElement("div");
         picContainer.className = "container";
         const pic = document.createElement('img');
@@ -35,7 +43,7 @@ const homePage = (() => {
         picContainer.appendChild(pic);
         content.appendChild(picContainer);
 
-
+        //Create div container for blurb and the blurb
         const blurbContainer = document.createElement("div");
         blurbContainer.className = "container";
         const blurb = document.createElement("p");
@@ -43,6 +51,8 @@ const homePage = (() => {
         blurbContainer.appendChild(blurb);
         content.appendChild(blurbContainer);
 
+
+        //Create div container for the hours and the list of hours
         const hoursContainer = document.createElement("div");
         hoursContainer.className = "container";
         const hoursHeader = document.createElement("h2");
@@ -57,6 +67,7 @@ const homePage = (() => {
 
     }
 
+    //Generates a list element based on array of times and returns it
     const generateHoursList = () => {
         const hoursList = document.createElement("ul");
         const hours = ["Monday: 8am - 5pm",
@@ -84,28 +95,144 @@ const homePage = (() => {
 })();
 
 const menuPage = (() => {
+
+    
+
+    const load = () => {
+        console.log("in menu load");
+        //Create div container for headline and the headline
+        const headlineContainer = document.createElement("div");
+        headlineContainer.className = "container";
+        const headline = document.createElement("h1");
+        headline.className = "headline";
+        headline.innerHTML = "Menu";
+        headlineContainer.appendChild(headline);
+        content.appendChild(headlineContainer);
+
+        const cheesePizza = newItem('Cheese Pizza',
+            'Handmade dough and tomato sauce with a blend of mozzarella and parmesan cheese',
+            '$12',
+            cheesePizzaPic);
+        const pepPizza = newItem('Pepperoni Pizza',
+            'Handmade dough and tomato sauce with a blend of mozzarella and parmesan cheese, topped with pepperoni',
+            '$14',
+            pepPizzaPic);
+        const meatPizza = newItem('Cheese Pizza',
+            'Handmade dough and tomato sauce with a blend of mozzarella and parmesan cheese, topped with perpperoni, sausage and bacon',
+            '$16',
+            meatPizzaPic);
+
+        content.appendChild(cheesePizza);
+        content.appendChild(pepPizza);
+        content.appendChild(meatPizza);
+    }
+
+    const newItem = (name, desc, price, pic) => {
+        const itemContainer = document.createElement("div");
+        itemContainer.className = "container";
+        const itemName = document.createElement("h3");
+        itemName.innerHTML = name;
+        const itemDesc = document.createElement("p");
+        itemDesc.innerHTML = desc;
+        const itemPrice = document.createElement("h4");
+        itemPrice.innerHTML = price;
+        const itemPic = document.createElement("img");
+        itemPic.setAttribute('src', pic);
+        itemPic.setAttribute('width', '100%');
+        itemContainer.appendChild(itemName);
+        itemContainer.appendChild(itemDesc);
+        itemContainer.appendChild(itemPic);
+        itemContainer.appendChild(itemPrice);
+
+        return itemContainer;
+
+    }
+
+    return{
+        load,
+    }
+
+
+
     
 
 })();
 
+
 const contactPage = (() => {
+
+    
+
+    const load = () => {
+        //Create header container and header
+        const headlineContainer = document.createElement("div");
+        headlineContainer.className = "container";
+        const headline = document.createElement("h1");
+        headline.className = "headline";
+        headline.innerHTML = "Contact Us";
+        headlineContainer.appendChild(headline);
+        content.appendChild(headlineContainer);
+
+        //Create div container for phone and email
+        const contactContainer = document.createElement("div");
+        contactContainer.className = "container";
+        const contactHeader = document.createElement("h2");
+        contactHeader.innerHTML = "Contact Info"
+        const contactInfo = document.createElement("p");
+        contactInfo.innerHTML = "<strong>Tel:</strong>555-555-5555</br><strong>Email:</strong>fakeemail@email.com";
+        contactContainer.appendChild(contactHeader);
+        contactContainer.appendChild(contactInfo);
+        content.appendChild(contactContainer);
+
+        //Create div container for address
+        const addressContainer = document.createElement("div");
+        addressContainer.className = "container";
+        const addressHeader = document.createElement("h2");
+        addressHeader.innerHTML = "Address Info"
+        const addressInfo = document.createElement("p");
+        addressInfo.innerHTML = "555 Fake St.</br>Fake Town, FK, 55555";
+        addressContainer.appendChild(addressHeader);
+        addressContainer.appendChild(addressInfo);
+        content.appendChild(addressContainer);
+        
+
+
+
+
+    }
+
+
+    return {
+        load,
+    }
 
 
 })();
 
 
-function tabSwitch(page){
+
+function tabSwitch(e){
+    console.log("in switch");
+    let page = e.target.id;
+    console.log(page);
     switch (page){
-        case "home":
+        case "tab-home":
+            content.innerHTML = '';
             homePage.load();
             break;
-        case "menu":
+        case "tab-menu":
+            content.innerHTML = '';
             menuPage.load();
             break;
-        case "contact":
+        case "tab-contact":
+            content.innerHTML = '';
             contactPage.load();
             break;
     }
 
 }
+
 homePage.load();
+document.getElementById('tab-home').addEventListener('click', tabSwitch);
+document.getElementById('tab-menu').addEventListener('click', tabSwitch);
+document.getElementById('tab-contact').addEventListener('click', tabSwitch);
